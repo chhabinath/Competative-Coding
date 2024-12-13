a6994fbdda7c12b307ba73ee408d9ab3fb139d18
Chhabinath Sahoo <chhabinath1519@gmail.com>
2024-12-14 01:44:58 +0530

    Time: 411 ms (82.28%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,8 +1,3 @@
--- We are selecting unique author IDs and renaming them as "id"
-SELECT DISTINCT author_id AS id 
--- We are looking at a table called "Views"
-FROM Views 
--- We are only interested in rows where the author ID matches the viewer ID
-WHERE author_id = viewer_id 
--- We want the results to be sorted in ascending order based on the author IDs
-ORDER BY id ASC;
+# Write your MySQL query statement below
+
+select distinct author_id as id from views where author_id = viewer_id order by author_id;
